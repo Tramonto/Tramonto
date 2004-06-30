8e11e04c126e7e1f90d06e64c46dd79398186549
mheroux <>
2004-06-30 21:57:00 +0000

    Working version of dft_schur_solver.  Still slow in some of the setup, but gets the right answers.

--- src/NEW/dft_schur_solver.cpp
+++ src/NEW/dft_schur_solver.cpp
@@ -37,6 +37,8 @@
 #include "Epetra_LinearProblem.h"
 #include "Epetra_Import.h"
 #include "AztecOO.h"
+#include "EpetraExt_RowMatrixOut.h"
+#include "Epetra_Time.h"
 #include "dft_schur_epetra_operator.h"
 #include "dft_globals_const.h"
 #include "rf_allo.h"
@@ -49,16 +51,13 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
 			     int * extern_index, int * data_org, int N_update, 
 			     int Nunk_per_node,
 			     DFT_SCHUR_SOLVER ** schur_solver) {
-  bool debug = true;
+
+  Epetra_MpiComm comm(MPI_COMM_WORLD);
+  Epetra_Time timer(comm);
+  bool debug = false;
 
   int nnodes = N_update/Nunk_per_node;
   assert(nnodes*Nunk_per_node==N_update); // Make sure there is a constant number of unknowns per node
-
-  int n2 = 6;
-  int n1 = Nunk_per_node-n2;
-  int nforward = n1/2;
-  int nrows1 = n1*2*nnodes;
-  int nrows2 = N_update - nrows1;
 
   int nexternal = data_org[AZ_N_external];
   int * ColGIDs = new int[N_update+nexternal];
@@ -71,11 +70,38 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
 
   int * node_reorder = new int[Nunk_per_node];
   
+  /* These parameter values are for the polymer problems
+  int n2 = 6;
+  int n1 = Nunk_per_node-n2;
+  int nforward = n1/2;
   for (int i=0; i<nforward; i++) {
     node_reorder[i] = n2 + i*2;
     node_reorder[i] = Nunk_per_node - i*2;
   }
+
   for (int i=0; i<n2; i++) node_reorder[n1+i] = i;
+  */
+
+  // These parameters are for a hard sphere with implicit nonlocal densities.
+  int n2 = 1;
+  int n1 = Nunk_per_node-n2;
+  node_reorder[0] = 1;
+  node_reorder[1] = 2;
+  node_reorder[2] = 5;
+  node_reorder[3] = 6;
+  node_reorder[4] = 7;
+  node_reorder[5] = 3;
+  node_reorder[6] = 4;
+  node_reorder[7] = 8;
+  node_reorder[8] = 9;
+  node_reorder[9] = 10;
+  node_reorder[10] = 0;
+
+  //Common code, using node_order as defined above
+  
+  int nrows1 = n1*nnodes;
+  int nrows2 = N_update - nrows1;
+
 
   int * reorder = new int[N_update];
   int * reorder_cols1 = new int[nrows1+nexternal];
@@ -84,7 +110,7 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
   for (int j=0; j<nnodes; j++) {
     int jinc = update[j*Nunk_per_node];
     for (int i=0; i<Nunk_per_node; i++) {
-      reorder[jinc+i] = jinc + node_reorder[i];
+      reorder[nnodes*i+j] = jinc + node_reorder[i];
       assert(update[j*Nunk_per_node+i]==update[j*Nunk_per_node]+i); // Check to see that GIDs at a node are contiguously ordered
     }
   }
@@ -108,17 +134,17 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
   int ncols2 = nrows2 + nexternal2;
       
 
-  Epetra_MpiComm comm(MPI_COMM_WORLD);
   Epetra_Map  RowMap1(-1, nrows1, reorder, 0, comm);
   Epetra_Map  RowMap2(-1, nrows2, reorder+nrows1, 0, comm);
   Epetra_Map  ColMap1(-1, ncols1, reorder_cols1, 0, comm);
   Epetra_Map  ColMap2(-1, ncols2, reorder_cols2, 0, comm);
-  Epetra_Map  RowMap(-1, N_update, update, 0, comm);
-  Epetra_Map  ColMap(-1, N_update+nexternal, ColGIDs, 0, comm);
   Epetra_CrsMatrix * A11 = new Epetra_CrsMatrix(Copy, RowMap1, ColMap1, 0);
   Epetra_CrsMatrix * A12 = new Epetra_CrsMatrix(Copy, RowMap1, ColMap2, 0);
   Epetra_CrsMatrix * A21 = new Epetra_CrsMatrix(Copy, RowMap2, ColMap1, 0);
   Epetra_CrsMatrix * A22 = new Epetra_CrsMatrix(Copy, RowMap2, ColMap2, 0);
+
+  Epetra_Map  RowMap(-1, N_update, update, 0, comm);
+  Epetra_Map  ColMap(-1, N_update+nexternal, ColGIDs, 0, comm);
   Epetra_CrsMatrix * A = new Epetra_CrsMatrix(Copy, RowMap, ColMap, 0);
 
   int * curColGIDs = new int[N_update+nexternal+1];
@@ -130,10 +156,10 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
     int * curEntries = bindx+bindx[i];
     double * curVals = val+bindx[i];
 
-    for (int j=0; j<curNumEntries; j++) curGlobalVals[j] = curVals[i];
+    for (int j=0; j<curNumEntries; j++) curGlobalVals[j] = curVals[j];
     curGlobalVals[curNumEntries] = val[i]; // Insert diagonal value
 
-    for (int j=0; j<curNumEntries; j++) curColGIDs[j] = ColGIDs[curEntries[i]];
+    for (int j=0; j<curNumEntries; j++) curColGIDs[j] = ColGIDs[curEntries[j]];
     curColGIDs[curNumEntries] = curRowGID; // Insert diagonal index
 
     if (RowMap1.MyGID(curRowGID)) {
@@ -152,6 +178,10 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
   A->FillComplete();
   A12->FillComplete(RowMap2, RowMap1);
   A21->FillComplete(RowMap1, RowMap2);
+  A11->OptimizeStorage();
+  A12->OptimizeStorage();
+  A21->OptimizeStorage();
+  A22->OptimizeStorage();
 
   if (debug) assert(A11->LowerTriangular());
 
@@ -169,6 +199,8 @@ void dft_create_schur_solver(int * proc_config, int * external, int * bindx,
   delete [] reorder_cols2;
   delete [] curColGIDs;
   delete [] curGlobalVals;
+
+  cout << "Time in dft_create_schur_solver = " << timer.ElapsedTime();
   return;
 }
 
@@ -180,6 +212,8 @@ void dft_update_schur_solver(int * bindx, double * val,
   Epetra_CrsMatrix * A21 = (Epetra_CrsMatrix *) schur_solver->A21;
   Epetra_CrsMatrix * A22 = (Epetra_CrsMatrix *) schur_solver->A22;
   Epetra_CrsMatrix * A   = (Epetra_CrsMatrix *) schur_solver->A;
+
+  Epetra_Time timer(A11->Comm());
 
   int * update = A->RowMap().MyGlobalElements();
   int * ColGIDs = A->ColMap().MyGlobalElements();
@@ -197,10 +231,10 @@ void dft_update_schur_solver(int * bindx, double * val,
     int * curEntries = bindx+bindx[i];
     double * curVals = val+bindx[i];
 
-    for (int j=0; j<curNumEntries; j++) curGlobalVals[j] = curVals[i];
+    for (int j=0; j<curNumEntries; j++) curGlobalVals[j] = curVals[j];
     curGlobalVals[curNumEntries] = val[i]; // Insert diagonal value
 
-    for (int j=0; j<curNumEntries; j++) curColGIDs[j] = ColGIDs[curEntries[i]];
+    for (int j=0; j<curNumEntries; j++) curColGIDs[j] = ColGIDs[curEntries[j]];
     curColGIDs[curNumEntries] = curRowGID; // Insert diagonal index
 
     if (RowMap1.MyGID(curRowGID)) {
@@ -216,10 +250,13 @@ void dft_update_schur_solver(int * bindx, double * val,
   delete [] curColGIDs;
   delete [] curGlobalVals;
 
+  cout << "Time in dft_update_schur_solver = " << timer.ElapsedTime();
   return;
 }
 
 void dft_apply_schur_solver(DFT_SCHUR_SOLVER * schur_solver, double * x_aztec, double * b_aztec) {
+
+  bool debug = false;
 
   Epetra_CrsMatrix * A11 = (Epetra_CrsMatrix *) schur_solver->A11;
   Epetra_CrsMatrix * A12 = (Epetra_CrsMatrix *) schur_solver->A12;
@@ -227,8 +264,31 @@ void dft_apply_schur_solver(DFT_SCHUR_SOLVER * schur_solver, double * x_aztec, d
   Epetra_CrsMatrix * A22 = (Epetra_CrsMatrix *) schur_solver->A22;
   Epetra_CrsMatrix * A   = (Epetra_CrsMatrix *) schur_solver->A;
 
+  Epetra_Time timer(A11->Comm());
+
+  if (debug) {
+    EpetraExt::RowMatrixToMatrixMarketFile("A11.mm", *A11, "A11 Matrix",
+					   "A11 matrix from Tramonto Schur complement");
+    EpetraExt::RowMatrixToMatrixMarketFile("A12.mm", *A12, "A12 Matrix",
+					   "A12 matrix from Tramonto Schur complement");
+    EpetraExt::RowMatrixToMatrixMarketFile("A21.mm", *A21, "A21 Matrix",
+					   "A21 matrix from Tramonto Schur complement");
+    EpetraExt::RowMatrixToMatrixMarketFile("A22.mm", *A22, "A22 Matrix",
+					   "A22 matrix from Tramonto Schur complement");
+    EpetraExt::RowMatrixToMatrixMarketFile("A.mm", *A, "A Matrix",
+					   "A matrix from Tramonto Schur complement");
+  }
   Epetra_Vector x(View, A->DomainMap(), x_aztec);
   Epetra_Vector b(View, A->RangeMap(), b_aztec);
+
+  if (debug) {
+    Epetra_Vector resid(b.Map());
+    A->Multiply(false, x, resid);
+    resid.Update(1.0, b, -1.0);
+    double resval;
+    resid.Norm2(&resval);
+    cout << "Initial residual before Schur Solver = "<<resval<< endl;
+  }
 
   Epetra_Map RowMap(A->RowMap());
   Epetra_Map RowMap1(A11->RowMap());
@@ -253,11 +313,23 @@ void dft_apply_schur_solver(DFT_SCHUR_SOLVER * schur_solver, double * x_aztec, d
   Epetra_LinearProblem problem(&op, &x2, &b2s);
   AztecOO solver(problem);
   solver.SetPrecMatrix(A22);
-  solver.SetAztecParam(AZ_precond, AZ_ilut);
-  solver.Iterate(200, 1.0E-10);
+  solver.SetAztecOption(AZ_precond, AZ_Jacobi);
+  solver.Iterate(200, 1.0E-7);
 
   op.ComputeX1(b1, x2, x1);
   
+  x.Export(x1, importer1, Insert);
+  x.Export(x2, importer2, Insert);
+
+  if (debug) {
+    Epetra_Vector resid(b.Map());
+    A->Multiply(false, x, resid);
+    resid.Update(1.0, b, -1.0);
+    double resval;
+    resid.Norm2(&resval);
+    cout << "Residual from Schur Solver = "<<resval<< endl;
+  }
+  cout << "Time in dft_update_schur_solver = " << timer.ElapsedTime();
 }
 void dft_destroy_schur_solver(DFT_SCHUR_SOLVER ** schur_solver) {
 
