18354e519a0123c78c6479069909404ce0aa936d
mheroux <>
2005-04-08 13:37:13 +0000

    ----------------------------------------------------------------------  Modified Files:  	dft_2x2_schur_epetra_operator.cpp  Added Files:  	dft_BlockLinearProblem.cpp dft_BlockLinearProblem.hpp  ----------------------------------------------------------------------
    Added a class to support easy handling of a block linear problem.

--- src/NEW/dft_2x2_schur_epetra_operator.cpp
+++ src/NEW/dft_2x2_schur_epetra_operator.cpp
@@ -69,67 +69,6 @@ int dft_2x2_schur_epetra_operator::Apply(const Epetra_MultiVector& X, Epetra_Mul
   Y.Update(-1.0, Y2, 1.0);
   return(0);
 }
-//==============================================================================
-double dft_2x2_schur_epetra_operator::ComputeGlobalResidual(const Epetra_Operator & A11, 
-							    const Epetra_MultiVector& B1, const Epetra_MultiVector& B2, 
-							    const Epetra_MultiVector& X1, const Epetra_MultiVector& X2) const {
-
-  int NumVectors = X1.NumVectors();
-  assert(NumVectors==X2.NumVectors());
-  assert(NumVectors==B1.NumVectors());
-  assert(NumVectors==B2.NumVectors());
-  Epetra_MultiVector Y11(A11.RangeMap(), NumVectors);
-  Epetra_MultiVector Y12(A12_->RangeMap(), NumVectors);
-  Epetra_MultiVector Y21(A21_->RangeMap(), NumVectors);
-  Epetra_MultiVector Y22(A22_->RangeMap(), NumVectors);
-
-  EPETRA_CHK_ERR(A11.Apply(*X1, Y11));
-  EPETRA_CHK_ERR(A12->Apply(*X2, Y12));
-  EPETRA_CHK_ERR(A21->Apply(*X1, Y21));
-  EPETRA_CHK_ERR(A22->Apply(*X2, Y22));
-  
-  // Compute Y11 = B1 - Y11 - Y12
-  EPETRA_CHK_ERR(Y11.Update(1.0, B1, -1.0, Y12, -1.0));
-
-  // Compute Y22 = B2 - Y22 - Y21
-  EPETRA_CHK_ERR(Y22.Update(1.0, B2, -1.0, Y21, -1.0));
-
-  Epetra_SerialDenseVector res1(NumVectors);
-  Epetra_SerialDenseVector res2(NumVectors);
-
-  EPETRA_CHK_ERR(Y11.NormInf(res1.Values())); // res1 and res2 now contain the inf-norms of each vector in the multivector
-  EPETRA_CHK_ERR(Y22.NormInf(res2.Values()));
-
-  double residual = EPETRA_MAX(res1.NormInf(), res2.NormInf()); // Now get the max over all max's.  This is the value we return
-
-  return(residual);
-
-//==============================================================================
-int dft_2x2_schur_epetra_operator::ComputeGlobalRhs(const Epetra_Operator & A11, 
-							    const Epetra_MultiVector& X1, const Epetra_MultiVector& X2, 
-							    Epetra_MultiVector& B1, Epetra_MultiVector& B2) const {
-
-  int NumVectors = X1.NumVectors();
-  assert(NumVectors==X2.NumVectors());
-  assert(NumVectors==B1.NumVectors());
-  assert(NumVectors==B2.NumVectors());
-  Epetra_MultiVector Y11(A11.RangeMap(), NumVectors);
-  Epetra_MultiVector Y12(A12_->RangeMap(), NumVectors);
-  Epetra_MultiVector Y21(A21_->RangeMap(), NumVectors);
-  Epetra_MultiVector Y22(A22_->RangeMap(), NumVectors);
-
-  EPETRA_CHK_ERR(A11.Apply(*X1, Y11));
-  EPETRA_CHK_ERR(A12->Apply(*X2, Y12));
-  EPETRA_CHK_ERR(A21->Apply(*X1, Y21));
-  EPETRA_CHK_ERR(A22->Apply(*X2, Y22));
-  
-  // Compute B1 = Y11 + Y12
-  EPETRA_CHK_ERR(B1.Update(1.0, Y11, 1.0, Y12, 0.0));
-
-  // Compute B2 = Y22 + Y21
-  EPETRA_CHK_ERR(B2.Update(1.0, Y22, 1.0, Y21, 0.0));
-
-  return(0);
 
 //==============================================================================
 int dft_2x2_schur_epetra_operator::ComputeRHS(const Epetra_MultiVector& B1, const Epetra_MultiVector& B2, 
