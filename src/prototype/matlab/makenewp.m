c15d261481db90cc80014b2393c8d1315fa5fb67
mheroux <>
2005-05-24 17:44:20 +0000

    ----------------------------------------------------------------------  Added Files:  	makenewp.m makenewpnew.m makep.m run_gmres.m run_gmres_sc.m  ----------------------------------------------------------------------
    These files prototype the solver for  the 1D lipid problem.

---
+++ src/prototype/matlab/makenewp.m
@@ -0,0 +1,6 @@
+n=161;
+p1 = [1:n];
+prow = zeros(40*n,1);
+pcol = prow;
+prow = [ 4*n+p1 6*n+p1 8*n+p1 10*n+p1 12*n+p1 14*n+p1 16*n+p1 18*n+p1 20*n+p1 22*n+p1 24*n+p1 26*n+p1 28*n+p1 30*n+p1 32*n+p1 34*n+p1 36*n+p1 38*n+p1 39*n+p1 37*n+p1 35*n+p1 33*n+p1 31*n+p1 29*n+p1 27*n+p1 25*n+p1 23*n+p1 21*n+p1 19*n+p1 17*n+p1 15*n+p1 13*n+p1 11*n+p1 9*n+p1 7*n+p1 5*n+p1 2*n+p1 3*n+p1 p1 n+p1];
+pcol = [ 4*n+p1 6*n+p1 8*n+p1 10*n+p1 12*n+p1 14*n+p1 16*n+p1 18*n+p1 20*n+p1 22*n+p1 24*n+p1 26*n+p1 28*n+p1 30*n+p1 32*n+p1 34*n+p1 36*n+p1 38*n+p1 39*n+p1 37*n+p1 35*n+p1 33*n+p1 31*n+p1 29*n+p1 27*n+p1 25*n+p1 23*n+p1 21*n+p1 19*n+p1 17*n+p1 15*n+p1 13*n+p1 11*n+p1 9*n+p1 7*n+p1 5*n+p1 p1 n+p1 2*n+p1 3*n+p1];
---
+++ src/prototype/matlab/run_gmres_sc.m
@@ -0,0 +1,49 @@
+function [xg,flag,relres,iter,resvec] = run_gmres_sc(A,x,b)
+tol = 1e-12;  maxit = 1; restart=200;
+nbeads=18;
+nunk=40;
+n=161;
+nn=nunk*n;
+n1=36*n+1; % n1=size of 1,1 block
+[prow,pcol]=makenewpnew(); 
+Ap=A(prow,pcol);
+xp=x(pcol);
+bp=b(prow);
+A11=Ap(1:n1,1:n1);
+A22=Ap(n1+1:nn,n1+1:nn);
+A21=Ap(n1+1:nn,1:n1);
+A12=Ap(1:n1,n1+1:nn);
+x1=xp(1:n1);
+x2=xp(n1+1:nn);
+b1=bp(1:n1);
+b2=bp(n1+1:nn);
+bs=b2-A21*A11invfun(b1);
+[L,U,P]=luinc(A22,'0');
+[x2,flag,relres,iter,resvec] = gmres(@afun,bs,restart,tol,maxit,@mfun);
+xp(n1+1:nn) = x2;
+btmp=b1-A12*x2;
+xp(1:n1)= A11invfun(btmp);
+xg=xp(pcol);
+    function y2 = afun(x2)
+        xtmp=A12*x2;
+        xtmp=A11invfun(xtmp);
+        y2 = A22*x2 - A21*xtmp;
+    end
+ 
+    function y2 = mfun(r2)
+        y2=L\(P*r2);
+        y2=U\y2;
+    end
+    function z = A11invfun(w)
+        z=w; % init z
+        z(1:n)=A11(1:n,1:n)\z(1:n); % Do first solve separately (no matvec needed)
+        for i=2:2*nbeads
+            nstrt=(i-1)*n+1;
+            nstop=nstrt+n-1;
+            xtmp=z(1:nstrt-1); % get the known solution values
+            Amv=A11(nstrt:nstop,1:nstrt-1);
+            btmp=z(nstrt:nstop) - Amv*xtmp;
+            z(nstrt:nstop)=A11(nstrt:nstop,nstrt:nstop)\btmp;
+        end
+    end
+end
