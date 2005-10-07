2ff4bcc201304ef6b50562de1b262f4ce2fcef20
ljfrink <>
2005-10-07 21:24:08 +0000

    Added two new problems to test suite for the WTC functionals. LJDF

--- src/dft_newton.c
+++ src/dft_newton.c
@@ -112,8 +112,8 @@ int solve_problem(double **x, double **x2)
        break;
      case BOND_WTC:
      case CAVITY_WTC:
-      /* indnonlocaleq[count_indnonlocal++]=iunk; break; A11 Block doesn't work*/
-       densityeq[count_density++]=iunk; break; 
+       indnonlocaleq[count_indnonlocal++]=iunk; break;   /*....putting in A11 Block doesn't work*/
+      /* densityeq[count_density++]=iunk; break;*/  /* putting them in A22 block doesn't work either */
      } 
    }
    LinProbMgr_manager = dft_hardsphere_lin_prob_mgr_create(Nunk_per_node, Aztec.options, Aztec.params, MPI_COMM_WORLD);
