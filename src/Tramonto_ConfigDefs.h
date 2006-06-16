f084b17f5f7abdaa66d48c654521213d6eacd033
jmwille <>
2006-06-16 03:12:43 +0000

    ----------------------------------------------------------------------  Committing in .
     Added Files:
     	config/tac_arg_with_package.m4 src/Tramonto_ConfigDefs.h
     ----------------------------------------------------------------------
    I did not add these files when adding the --with-noxloca option.

---
+++ src/Tramonto_ConfigDefs.h
@@ -0,0 +1,21 @@
+
+//@HEADER
+/*
+************************************************************************
+
+************************************************************************
+*/
+//@HEADER
+
+#ifndef TRAMONTO_CONFIGDEFS_H
+#define TRAMONTO_CONFIGDEFS_H
+
+#ifdef HAVE_CONFIG_H
+
+#include <Tramonto_config.h>
+
+//#else /*HAVE_CONFIG_H is not defined*/
+
+#endif /*HAVE_CONFIG_H*/
+
+#endif /* TRAMONTO_CONFIGDEFS_H */
