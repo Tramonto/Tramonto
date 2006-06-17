a416446c4c859a38e9a660cf4dbc268a3c995df0
jmwille <>
2006-06-17 16:18:38 +0000

    ----------------------------------------------------------------------  Committing in .
     Modified Files:
     	src/Tramonto_ConfigDefs.h
     ----------------------------------------------------------------------
    Fixing a few warnings that Andy S was seeing.  Trilinos packages
    redefine the PACKAGE family of macros, so we simply undefine them
    before the redefinition since the macro values are not used anyway.
    
    This is the way that Trilinos packages have handled this problem for a
    few years.

--- src/Tramonto_ConfigDefs.h
+++ src/Tramonto_ConfigDefs.h
@@ -12,6 +12,40 @@
 
 #ifdef HAVE_CONFIG_H
 
+/*
+ * The macros PACKAGE, PACKAGE_NAME, etc, get defined for each Trilinos package
+ * and need to be undef'd here to avoid warnings due to redefinition.
+ * Kevin Long came up with this solution.
+ */
+
+#ifdef PACKAGE
+#undef PACKAGE
+#endif
+
+#ifdef PACKAGE_NAME
+#undef PACKAGE_NAME
+#endif
+
+#ifdef PACKAGE_BUGREPORT
+#undef PACKAGE_BUGREPORT
+#endif
+
+#ifdef PACKAGE_STRING
+#undef PACKAGE_STRING
+#endif
+
+#ifdef PACKAGE_TARNAME
+#undef PACKAGE_TARNAME
+#endif
+
+#ifdef PACKAGE_VERSION
+#undef PACKAGE_VERSION
+#endif
+
+#ifdef VERSION
+#undef VERSION
+#endif
+
 #include <Tramonto_config.h>
 
 //#else /*HAVE_CONFIG_H is not defined*/
