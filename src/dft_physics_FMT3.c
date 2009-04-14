bf2b92222b7c349743cd8d420d48dbc623df1966
alfrisc <>
2009-04-14 19:57:11 +0000

    Committing small bug fix (sign error).  -ALF

--- src/dft_physics_FMT3.c
+++ src/dft_physics_FMT3.c
@@ -61,7 +61,7 @@ double FMT3_energy_density(double *n)
 void FMT3_1stderiv(double *n,double DOT_12,double DOT_22,double *inv_n3, double *dphi_drb_loc)
 {
    int idim,iv1,iv2;
-;
+
    dphi_drb_loc[0] =  log(inv_n3[1]);
    dphi_drb_loc[1] =  n[2]*inv_n3[1];
 
@@ -136,25 +136,15 @@ struct RB_Struct d2phi_drb2_delta_rb_FMT3(double *n, int *offset, double *sign, 
      for (idim = 0; idim<Ndim; idim++) 
        vector[idim] = offset[idim] * Esize_x[idim] * Inv_rad[icomp];
 
-     /* ALF: possible error--shouldn't have a 1/4pi here in second term */
-     /*   tmp.S2 = (inv_n3[1]*Inv_4pir[icomp]
-	  + n[2]*fac2*Inv_4pi*inv_n3[2]/(6.*PI*n3sq));*/
        tmp.S2 = (inv_n3[1]*Inv_4pir[icomp]
          + n[2]*fac2*inv_n3[2]/(6.*PI*n3sq));
 
      for (idim = 0; idim<Ndim; idim++){
         i2v=Nrho_bar_s+Ndim+idim;
-   /* ALF: possible error--shouldn't have a 1/4pi here  */
-	/*   tmp.S2 += vector[idim]
-	 *n[i2v]*fac2*Inv_4pi*inv_n3[2]/(6.*PI*n3sq);*/
 	tmp.S2 += vector[idim]
               *n[i2v]*fac2*inv_n3[2]/(6.*PI*n3sq);
      }
 
-     /*  tmp.S3 = (Inv_4pirsq[icomp]*inv_n3[1] 
-         + Inv_4pir[icomp]*n[2]*inv_n3[2]
-         + Inv_4pi*n[1]*inv_n3[2]+(n2sq-DOT_22)*(
-	 (-2.*fac2*fac3) + (fac1*inv_n3[2]/n3sq))/(12.*PI)  );*/
        tmp.S3 = (Inv_4pirsq[icomp]*inv_n3[1] 
          + Inv_4pir[icomp]*n[2]*inv_n3[2]
          + n[1]*inv_n3[2]+(n2sq-DOT_22)*(
@@ -163,10 +153,6 @@ struct RB_Struct d2phi_drb2_delta_rb_FMT3(double *n, int *offset, double *sign, 
      for (idim = 0; idim<Ndim; idim++){
         i2v=Nrho_bar_s+Ndim+idim;
         i1v=Nrho_bar_s+idim;
-	/*  tmp.S3 += inv_n3[2] * vector[idim] * ( 
-               n[i2v]*Inv_4pir[icomp] + n[i1v]
-              + n[2]*n[i2v]*( 
-              (-2.*fac2*fac3) + (fac1*inv_n3[2]/n3sq))/(6.*PI) ) ;*/
 	tmp.S3 +=  vector[idim] * ( 
 	      n[i2v]*Inv_4pir[icomp]*inv_n3[2] + n[i1v]*inv_n3[2] 
               + n[2]*n[i2v]*( 
@@ -183,13 +169,11 @@ struct RB_Struct d2phi_drb2_delta_rb_FMT3(double *n, int *offset, double *sign, 
      for (idim = 0; idim<Ndim; idim++){
        i2v=Nrho_bar_s+Ndim+idim;
        i1v=Nrho_bar_s+idim;
-       /*  tmp.V2[idim] = sign[idim]*
-                    ( 2.*n[i2v]*Inv_4pi*inv_n3[2]*fac2/(12.*PI*n3sq)
-                    - (-Inv_4pir[icomp]*inv_n3[1] - n[2]*Inv_4pi*fac2*inv_n3[2]/(6.*PI*n3sq))*vector[idim]);*/
+
        tmp.V2[idim] = sign[idim]*
-                    ( n[i2v]*inv_n3[2]*fac2/(6.*PI*n3sq)
+                    ( -n[i2v]*inv_n3[2]*fac2/(6.*PI*n3sq)
                     - (-Inv_4pir[icomp]*inv_n3[1] - n[2]*fac2*inv_n3[2]/(6.*PI*n3sq))*vector[idim]);
-     }
+		}
   }
   else{
     tmp.S2 = 0.0;
@@ -246,13 +230,6 @@ struct RB_Struct d2phi_drb2_theta_rb_FMT3(double *n)
            n[1]*inv_n3[2] 
          + (n[2]*n[2]-DOT_22)*(fac1*inv_n3[2]/n3sq + 2.*fac2*fac3)/(12*PI));
 
-    /*  tmp.S3 = (
-           n[1]*inv_n3[2] 
-           + 2*(n[1]*n[2]-DOT_12)*inv_n3[3]
-           + ((n2cb-3.*n[2]*DOT_22)/(36.*PI))*(
-           + (((2.*log(1.-n[3])+1)*inv_n3[2])/n3sq) 
-           + (4.*fac1*fac3) + (6.*fac2*inv_n3[2]/n3_4th) 
-           + (inv_n3[4]/n3sq) )  );*/
       tmp.S3 = (
            n[0]*inv_n3[2] 
            + 2*(n[1]*n[2]-DOT_12)*inv_n3[3]
