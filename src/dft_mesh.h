d14a34efe68c2e2cd19584f59a0466a876f0b6ca
ljfrink <>
2010-06-29 19:00:21 +0000

    change order of az_aztec.h and az_aztec_defs.h include statements
    --L

--- src/dft_mesh.h
+++ src/dft_mesh.h
@@ -8,8 +8,8 @@
 #include <string.h>
 #endif
 #include "mpi.h"
-#include "az_aztec.h"
 #include "az_aztec_defs.h"
+#include "az_aztec.h"
 #include "rf_allo.h"
 #include "dft_basic_lin_prob_mgr_wrapper.h"
 #include "dft_poly_lin_prob_mgr_wrapper.h"
