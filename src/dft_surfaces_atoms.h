0e017aeaab40aa5d95cad993fc7f9d7427e3f6aa
ljfrink <>
2012-11-15 15:15:24 +0000

    removing dft_mesh_surfaces.c and dft_mesh_surfaces.h from repository
    LF

--- src/dft_surfaces_atoms.h
+++ src/dft_surfaces_atoms.h
@@ -32,40 +32,4 @@ extern double Sigma_ww[NWALL_MAX_TYPE][NWALL_MAX_TYPE];
 #endif
 #define COULOMB      1
 extern int Ipot_ff_c;
-typedef struct SurfaceGeom_Struct SurfaceGeom_Struct;
-extern struct SurfaceGeom_Struct *SGeom;
-#define NWALL_MAX 600 
-extern int Lwedge_cutout[NWALL_MAX];
-extern int Lperiodic_overlay[NWALL_MAX_TYPE];
-extern int Nperiodic_overlay[NWALL_MAX_TYPE];
-#define NPERIODIC_MAX 4
-extern int Llinear_overlay[NWALL_MAX_TYPE];
-extern int Nlinear_overlay[NWALL_MAX_TYPE];
-struct SurfaceGeom_Struct {
-  int       surfaceTypeID;    /* ID of the type of surface */
-  int       orientation;  /* orientation of the surface */
-  double    *halfwidth;   /* planar surface params given in halfwidth */
-  double    radius;       /* radius of spherical or cylindrical surface */
-  double    halflength;   /* length of finite cylinders and pores */
-  double    radius2;      /* a second radius for tapered pores or cylinders */
-  int       Lwedge_cutout;    /* TRUE or FALSE for wedge cutout from basic surfac */
-  double    angle_wedge_start;    /* angle as measured from x0 axis */
-  double    angle_wedge_end;    /* angle as measured from x0 axis */
-  int       Lrough_surface;    /* TRUE or FALSE for surface roughness */
-  double    roughness;          /* maximum roughness amplitude */
-  double    roughness_length;    /* lengthscale for the rougness */
-  int       Lperiodic_overlay;    /* TRUE or FALSE for periodic function added to surface */
-  int       Nperiodic_overlay;     /* The number of periodic functions to apply */
-  double    orientation_periodic[NPERIODIC_MAX];    /* maximum amplitude for a cosine wave superimposed on a surface */
-  double    amplitude[NPERIODIC_MAX];    /* maximum amplitude for a cosine wave superimposed on a surface */
-  double    wavelength[NPERIODIC_MAX];    /* desired wavelength of cosine wave superimposed on a surface */
-  double    origin_PeriodicFunc[NPERIODIC_MAX];     /* The origin of periodic functions to apply */
-  int       Llinear_overlay;    /* TRUE or FALSE for linear function added to surface */
-  int       Nlinear_overlay;     /* The number of linear functions to apply */
-  double    orientation_linear[NPERIODIC_MAX];    /* maximum amplitude for a linear function superimposed on a surface */
-  double    slope[NPERIODIC_MAX];    /* maximum amplitude for a linear function superimposed on a surface */
-  double    origin_LinearFunc[NPERIODIC_MAX];     /* The origin of linear functions to apply */
-  double    endpoint_LinearFunc[NPERIODIC_MAX];     /* The end point of linear functions to apply */
-  int    *ReflectionsAreIndependent;  /* TRUE or FALSE for treating special boundary conditions */
-};
 void surface_atoms_inSurfaceTest(int iwall,int iwall_type,double *fluid_testpos,double **wall_pos,double dist_adjustments,int flag_X_to_center,double *delr_vext,double *delr_zone,int *logical_inwall,int *logical_nearWallDielec);
