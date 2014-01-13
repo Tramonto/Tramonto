2966d39ae57855744f46b94d8d3acca8d5e8a75b
alfrisc <>
2014-01-13 21:39:40 +0000

    Small bug fix.  Code was giving a segmentation fault if one had volume charges and periodic boundary conditions, due to trying to print out Vext_coul when that didn't exist. -ALF

--- src/dft_main.c
+++ src/dft_main.c
@@ -308,7 +308,7 @@ void dftmain(double * engptr)
         print_vext(Vext,Vext_Filename);
         if (Restart_Vext == READ_VEXT_STATIC) print_vext(Vext_static,"dft_vext_static.dat");
         print_zeroTF(Zero_density_TF,ZeroTF_filename);
-        if (Vol_charge_flag && Ndim==3) print_vext(Vext_coul,VextCoul_Filename);
+        if (Vol_charge_flag && Ndim==3 && proper_bc) print_vext(Vext_coul,VextCoul_Filename);
      }
 
 
